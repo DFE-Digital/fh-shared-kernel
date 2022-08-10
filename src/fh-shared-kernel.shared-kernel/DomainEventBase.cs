b7443821a50cb5c2a5f4d2a20a9826c302810d49
Gary Griffiths <gary.griffiths@digital.education.gov.uk>
2022-08-10 10:52:05 +0100

    Initial version

---
+++ src/fh-shared-kernel.shared-kernel/DomainEventBase.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace LocalAuthorityInformationServices.SharedKernel;
+
+public abstract class DomainEventBase : INotification
+{
+    public DateTimeOffset DateOccurred { get; protected set; } = DateTimeOffset.UtcNow;
+}
