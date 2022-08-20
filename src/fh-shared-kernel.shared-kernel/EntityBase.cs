62df794dbcd64b5598499cb7b3b5355313b2beab
Gary Griffiths <gary.griffiths@digital.education.gov.uk>
2022-08-20 06:41:57 +0100

    Changed scope of RegisterDomainEvent to Public

--- src/fh-shared-kernel.shared-kernel/EntityBase.cs
+++ src/fh-shared-kernel.shared-kernel/EntityBase.cs
@@ -22,7 +22,7 @@ public abstract class EntityBase<Tid>
     [NotMapped]
     public IEnumerable<DomainEventBase> DomainEvents => _domainEvents.AsReadOnly();
 
-    protected void RegisterDomainEvent(DomainEventBase domainEvent) => _domainEvents.Add(domainEvent);
+    public void RegisterDomainEvent(DomainEventBase domainEvent) => _domainEvents.Add(domainEvent);
 
     internal void ClearDomainEvents() => _domainEvents.Clear();
 }
