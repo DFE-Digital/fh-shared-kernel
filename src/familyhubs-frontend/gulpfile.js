fb3e14677a8db3437cbe5f4c38708183cf858858
Phil Davies <github@phildavies.co.uk>
2023-03-28 13:03:11 +0100

    wip

--- src/familyhubs-frontend/gulpfile.js
+++ src/familyhubs-frontend/gulpfile.js
@@ -1,95 +1,28 @@
-/// <binding ProjectOpened='js:watch, sass-to-min-css:watch' />
 "use strict";
 
-var tsScriptsSrc = './scripts/**';
+const gulp = require("gulp"),
+    rename = require('gulp-rename'),
+    fs = require('fs');
 
-var gulp = require("gulp"),
-    sass = require('gulp-sass')(require('sass')),
-    sourcemaps = require('gulp-sourcemaps'),
-    csso = require('gulp-csso'),
-    terser = require('gulp-terser'),
-    ts = require("gulp-typescript"),
-    //typescript = require('typescript'),
-    rollup = require('gulp-better-rollup'),
-    //concat = require('gulp-concat'),
-    del = require('del'),
-    rename = require('gulp-rename');
+        //todo: postinstall
 
-gulp.task('sass-to-min-css', async function () {
-    return gulp.src('./styles/all.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(csso())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./wwwroot/css'));
+function copyPackageJsToWwwroot(packageName, srcFilename) {
+    // Read the package.json file to get the package version
+    const packageJson = JSON.parse(fs.readFileSync(`../${packageName}/package.json`));
+    const packageVersion = packageJson.version;
+
+    // Set the destination file name
+    const destFileName = `${packageName}-${packageVersion}.min.js`;
+
+    // Copy and rename the file
+    return gulp.src(`../${packageName}/${srcFileName}`)
+        .pipe(rename(destFileName))
+        .pipe(gulp.dest('../../wwwroot/js'));
+}
+
+gulp.task('copy-packages-js', function() {
+    copyPackageJsToWwwroot('govuk-frontend', 'all.js');
+    copyPackageJsToWwwroot('familyhubs-frontend', 'all.js');
 });
 
-gulp.task('sass-to-min-css:watch', function () {
-    gulp.watch('./styles/**', gulp.series('sass-to-min-css'));
-});
-
-// https://www.meziantou.net/compiling-typescript-using-gulp-in-visual-studio.htm
-
-//todo: clean to delete files in dest? & tmp folder
-
-var tsProject;
-
-gulp.task('transpile-ts', function () {
-
-    if (!tsProject) {
-        tsProject = ts.createProject({
-            noImplicitAny: false,
-            noEmitOnError: true,
-            removeComments: false,
-            target: "es6",
-            allowJs: true,
-            checkJs: true
-        });
-    }
-
-    //console.log(`TypeScript version: ${typescript.version}`);
-
-    var reporter = ts.reporter.fullReporter();
-
-    var tsResult = gulp.src(tsScriptsSrc)
-        .pipe(sourcemaps.init())
-        .pipe(tsProject(reporter));
-
-    return tsResult.js
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest("./tmp/js"));
-});
-
-//todo: try rollup-plugin-preserve-modules to preserve the order of js files (if including govuk)
-//todo: any benefit of using rollup-plugin-terser?
-
-//gulp.task('naive-bundle-js', () => {
-//    return gulp.src(['./wwwroot/lib/govuk/assets/js/govuk-4.4.1.js', './tmp/js/app.js'])
-//        .pipe(sourcemaps.init())
-//        .pipe(concat('bundle.js'))
-//        // inlining the sourcemap into the exported .js file
-//        .pipe(sourcemaps.write())
-//        .pipe(gulp.dest('./tmp/js'));
-//});
-
-gulp.task('bundle-and-minify-js', () => {
-//    return gulp.src('./tmp/js/bundle.js')
-    return gulp.src('./tmp/js/familyhubs.js')
-        .pipe(sourcemaps.init())
-        .pipe(rollup({}, 'es'))
-        .pipe(terser())
-        .pipe(rename({ basename: 'all', suffix: '.min' }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('.'));
-});
-
-gulp.task('clean', () => {
-    return del('./tmp/**');
-});
-
-//gulp.task('js', gulp.series('clean', 'transpile-ts', 'naive-bundle-js', 'bundle-and-minify-js'));
-gulp.task('js', gulp.series('clean', 'transpile-ts', 'bundle-and-minify-js'));
-
-gulp.task('js:watch', function () {
-    gulp.watch(tsScriptsSrc, gulp.series('js'));
-});
+        //todo: delegate from consumer gulp to this gulp?
