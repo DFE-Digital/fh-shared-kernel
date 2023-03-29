e96c1e27bdab6f09fe99cd5559728a717a081819
Phil Davies <github@phildavies.co.uk>
2023-03-29 13:39:40 +0100

    wip

--- src/familyhubs-frontend/scripts/familyhubs.ts
+++ src/familyhubs-frontend/scripts/familyhubs.ts
@@ -41,15 +41,4 @@ window.FamilyHubsFrontend.initAll = () => {
             window.history.back();
         });
     });
-
-    //todo: when plug into find, don't forget to plug this in
-    //const button = document.getElementById('open-close-filters');
-    //button?.addEventListener('click', function handleClick(event) {
-    //    const filterButton = document.getElementById("filters") as HTMLDivElement | null;
-    //    if (filterButton.style.display === "none") {
-    //        filterButton.style.display = "block";
-    //    } else {
-    //        filterButton.style.display = "none";
-    //    }
-    //});
 };
