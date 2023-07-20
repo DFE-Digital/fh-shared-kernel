6b53ab847a66e90c91c21ee1f491593a3a584abb
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-07-20 13:22:59 +0100

    FHG-3939 Api Exception Response

---
+++ src/fh-shared-kernel.shared-kernel/Exceptions/ApiExceptionResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FamilyHubs.SharedKernel.Exceptions
+{
+    public class ApiExceptionResponse
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = string.Empty;
+
+        [JsonPropertyName("status")]
+        public int StatusCode { get; set; }
+
+        [JsonPropertyName("detail")]
+        public string Detail { get; set; } = string.Empty;
+
+        [JsonPropertyName("errors")]
+        public IEnumerable<object> Errors { get; set; } = new List<object>();
+
+        [JsonPropertyName("errorCode")]
+        public string ErrorCode { get; set; } = string.Empty;
+
+    }
+}
