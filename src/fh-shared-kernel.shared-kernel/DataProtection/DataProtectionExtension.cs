6dd92d6d4a234eeda26c56db53a75a4909be4038
Phil Davies <github@phildavies.co.uk>
2023-08-01 15:03:29 +0100

    wip

--- src/fh-shared-kernel.shared-kernel/DataProtection/DataProtectionExtension.cs
+++ src/fh-shared-kernel.shared-kernel/DataProtection/DataProtectionExtension.cs
@@ -3,8 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Azure.Identity;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
-using FamilyHubs.SharedKernel.EntityFramework;
-using Azure.Core;
 
 namespace FamilyHubs.SharedKernel.DataProtection;
 
@@ -15,7 +13,7 @@ namespace FamilyHubs.SharedKernel.DataProtection;
 /// </remarks>
 public static class DataProtectionExtension
 {
-    public static void AddDataProtection(this IServiceCollection services, IConfiguration configuration, string appName)
+    public static void AddFamilyHubsDataProtection(this IServiceCollection services, IConfiguration configuration, string appName)
     {
         // Add a DbContext to store your Database Keys
         services.AddDbContext<DataProtectionKeysContext>(options =>
@@ -23,14 +21,9 @@ public static class DataProtectionExtension
                 configuration.GetConnectionString("DataProtectionKeysConnection"),
                 ob => ob.MigrationsAssembly(typeof(DataProtectionKeysContext).Assembly.ToString())));
 
-        using (var scope = services.BuildServiceProvider().CreateScope())
-        {
-            var dbContext = scope.ServiceProvider.GetRequiredService<DataProtectionKeysContext>();
-            MigrationUtility.ApplyMigrations(dbContext);
-        }
-
         // reuse same config? have an interface that the config section implements, then have a config to give the section name?
         // put both in own section??
+        // we need the kv id to be a valid uri, so we can't reuse this..
         string? keyVaultIdentifier = configuration["Crypto:KeyVaultIdentifier"];
         if (string.IsNullOrEmpty(keyVaultIdentifier))
         {
