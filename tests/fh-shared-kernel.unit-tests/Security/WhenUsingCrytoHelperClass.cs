54562b94fddcf0e371654f6725d3ccec0e81c1cb
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-09-19 11:19:51 +0100

    Added Access to DbEncryptionKey and DbEncryptionIVKey

--- tests/fh-shared-kernel.unit-tests/Security/WhenUsingCrytoHelperClass.cs
+++ tests/fh-shared-kernel.unit-tests/Security/WhenUsingCrytoHelperClass.cs
@@ -1,6 +1,6 @@
 ﻿using FamilyHubs.SharedKernel.Security;
 using FluentAssertions;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.DataEncryption.Providers;
 using Moq;
 using System.Security.Cryptography;
 
@@ -13,11 +13,13 @@ public class WhenUsingCrytoHelperClass
     public async Task ThenEncryptStringAndThenDecryptItBack()
     {
         //Arrange
-        (string publicKey, string privateKey) = GenerateKeys();
+        (string publicKey, string privateKey, string dbEncryptionKey, string dbEncryptionIVKey) = GenerateKeys();
 
         Mock<IKeyProvider> provider = new Mock<IKeyProvider>();
         provider.Setup(x => x.GetPublicKey()).ReturnsAsync(publicKey);
         provider.Setup(x => x.GetPrivateKey()).ReturnsAsync(privateKey);
+        provider.Setup(x => x.GetDbEncryptionKey()).ReturnsAsync(dbEncryptionKey);
+        provider.Setup(x => x.GetDbEncryptionIVKey()).ReturnsAsync(dbEncryptionIVKey);
 
         string expected = "Hello, RSA encryption!";
         ICrypto crypto = new Crypto(provider.Object);
@@ -30,16 +32,34 @@ public class WhenUsingCrytoHelperClass
         expected.Should().Be(decryptedData);
         expected.Should().NotBe(encryptedData);
         encryptedData.Length.Should().BeGreaterThan(expected.Length);
+        dbEncryptionKey.Should().NotBeNullOrEmpty();
+        dbEncryptionIVKey.Should().NotBeNullOrEmpty();
     }
     
 
-    public (string publicKey, string privateKey) GenerateKeys()
+    public (string publicKey, string privateKey, string dbEncryptionKey, string dbEncryptionIVKey) GenerateKeys()
     {
         using (var rsa = RSA.Create())
         {
             string publicKey = rsa.ToXmlString(false);
             string privateKey = rsa.ToXmlString(true);
-            return (publicKey, privateKey);
+
+            var result = AesProvider.GenerateKey(AesKeySize.AES256Bits);
+            List<string> items = new List<string>();
+            foreach (var item in result.IV)
+            {
+                items.Add(item.ToString());
+            }
+            string dbEncryptionKey = string.Join(',', items.ToArray());
+            items.Clear();
+
+            foreach (var item in result.Key)
+            {
+                items.Add(item.ToString());
+            }
+            string dbEncryptionIVKey = string.Join(',', items.ToArray());
+
+            return (publicKey, privateKey, dbEncryptionKey, dbEncryptionIVKey);
         }
     }
 }
